4c8c39a5f5b24ee1c3ed46f355a81b47f6e7db26
B0elena <e.mc2.1012md@gmail.com>
2020-09-21 13:41:05 +0900

    deviseの導入と２つのモデルを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  sessions:      'admins/sessions',
+  passwords:     'admins/passwords',
+  registrations: 'admins/registrations'
+}
+devise_for :users, controllers: {
+  sessions:      'users/sessions',
+  passwords:     'users/passwords',
+  registrations: 'users/registrations'
+}
   root to: "tweets#index"
 end
