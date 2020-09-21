f67deb72be47194e73d5150b64a885ba0617ce99
B0elena <e.mc2.1012md@gmail.com>
2020-09-21 16:39:02 +0900

    userとadminで別々にログアウトできるように作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
+  end
+  private
+  def move_to_index
+    unless (user_signed_in? || admin_signed_in?)
+      redirect_to action: :index
+    end
   end
 end
