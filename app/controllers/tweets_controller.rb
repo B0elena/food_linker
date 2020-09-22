89123745e785f71407de94fdf063e7bd8c8b8b72
B0elena <e.mc2.1012md@gmail.com>
2020-09-22 18:43:38 +0900

    投稿の編集機能実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,6 +30,20 @@ class TweetsController < ApplicationController
     end
   end
 
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    @tweet = Tweet.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
+  end
+
+
   private
   def move_to_index
     unless (user_signed_in? || admin_signed_in?)
