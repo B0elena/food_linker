096d0a77b4a08aa0f7343552ef7c4c2cbc689208
B0elena <e.mc2.1012md@gmail.com>
2020-09-24 12:44:09 +0900

    commentsテーブルの作成とコントローラーの作成

---
+++ db/migrate/20200924033557_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.string :comment_text, null: false
+      t.references :tweet,    null: false, foreign_key: true
+      t.references :user,     null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
