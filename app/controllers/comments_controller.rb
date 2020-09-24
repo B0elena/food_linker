3804458ea50de9a7f8c9b5ca57d809d96307a46b
B0elena <e.mc2.1012md@gmail.com>
2020-09-24 15:00:52 +0900

    コメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,11 @@
 class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to tweet_path(comment.tweet.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment_text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
 end
